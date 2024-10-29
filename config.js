cceb797d62aca48d2c64e07d6a58e8606d2beb28
Sreejit sarkar <107984412+Official-Rup@users.noreply.github.com>
2024-10-29 18:46:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "fCwWwRJA#B75g6tUQdTNdlTWXKxZTHVl9YFBBsQZ8yQOJrevg9xA",
 MONGODB: process.env.MONGODB || "mongodb+srv://suhail:suhail@clustere.tuhzpez.mongodb.net/?retryWrites=true&w=majority",
 };
